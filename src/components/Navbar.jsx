7025aa4aa4e517744fa198dfef25901049fc76ff
ashaansojib <ashaduzzamansojib67@gmail.com>
2023-09-08 20:21:26 +0600

    user profie create

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../auth/AuthProvider';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+    const { user, logOut } = useContext(AuthContext)
+    const handleLogOut = () => {
+        logOut()
+            .then(() => { })
+            .catch(error => { })
+    }
     return (
-        <nav className="navbar border-b border-slate-300">
+        <nav className="navbar border-b border-slate-300 justify-between">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -38,8 +45,37 @@ const Navbar = () => {
                     <li><a>Item 3</a></li>
                 </ul>
             </div>
-            <div className="navbar-end">
-                <Link to="/dashboard" className="btn btn-ghost normal-case">Dashboard</Link>
+            <div className="dropdown dropdown-end">
+                <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                    <div className="w-10 rounded-full">
+                        <img src={user?.photoURL} />
+                    </div>
+                </label>
+                <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+                    <li>
+                        <button onClick={() => document.getElementById('my_modal_5').showModal()} className="justify-between">
+                            Profile
+                        </button>
+                    </li>
+                    <li><Link to="/dashboard">Dashboard</Link></li>
+                    <li><button onClick={handleLogOut}>Logout</button></li>
+                </ul>
+                <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+                    <div className="modal-box">
+                        <figure className='flex justify-center'>
+                            <img src={user?.photoURL} alt="Shoes" className="rounded-xl w-[100px] h-[100px]" />
+                        </figure>
+                        <div className="text-center">
+                            <h2 className="text-2xl font-semibold">{user?.displayName}</h2>
+                            <p>Programmer can anything new that who didn't done yet?</p>
+                        </div>
+                        <div className="modal-action">
+                            <form method="dialog">
+                                <button className="btn">Close</button>
+                            </form>
+                        </div>
+                    </div>
+                </dialog>
             </div>
         </nav>
     );
